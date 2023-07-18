41aec6677bf05b0852ec5529e919db990a6cc632
slince-zero <1711257704@qq.com>
2023-07-18 17:32:48 +0800

    表单

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,10 +1,106 @@
 <template>
-  <h1>我是User</h1>
+  <div class="manage">
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="50%">
+      <!-- 用户的表单信息 -->
+      <el-form ref="form" :rules="rules" :inline="true" :model="form" label-width="80px">
+        <el-form-item label="姓名" prop="name">
+          <el-input placeholder="请输入姓名" v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄" prop="age">
+          <el-input placeholder="请输入年龄" v-model="form.age"></el-input>
+        </el-form-item>
+        <el-form-item label="性别" prop="sex">
+          <el-select v-model="form.sex" placeholder="请选择性别">
+            <el-option label="男" value="1"></el-option>
+            <el-option label="女" value="0"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="出生日期" prop="birth">
+          <el-col :span="11">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="form.birth"
+              style="width: 100%"
+            ></el-date-picker>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="地址" prop="addr">
+          <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submit"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
+
+    <div class="manage-header">
+      <el-button type="text" @click="dialogVisible = true">+ 新增</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'User',
+    data() {
+      return {
+        dialogVisible: false,
+        form: {
+          name: '',
+          age: '',
+          sex: '',
+          birth: '',
+          addr: '',
+        },
+        rules: {
+          name: [
+            {
+              required: true,
+              message: '请输入姓名',
+            },
+          ],
+          age: [
+            {
+              required: true,
+              message: '请输入年龄',
+            },
+          ],
+          sex: [
+            {
+              required: true,
+              message: '请选择性别',
+            },
+          ],
+          birth: [
+            {
+              required: true,
+              message: '请选择日期',
+            },
+          ],
+          addr: [
+            {
+              required: true,
+              message: '请输入地址',
+            },
+          ],
+        },
+      }
+    },
+    methods:{
+      // 提交用户表单
+      submit(){
+        this.$refs.form.validate((val)=>{
+          if(val){
+            console.log(this.form);
+          }
+        })
+      }
+    }
   }
 </script>
 
