b81f31d1fcb228c025bef9e5d21c4d648000fb83
slince-zero <1711257704@qq.com>
2023-07-19 18:17:01 +0800

    用户列表增删改完成

--- src/views/User.vue
+++ src/views/User.vue
@@ -18,8 +18,8 @@
         </el-form-item>
         <el-form-item label="性别" prop="sex">
           <el-select v-model="form.sex" placeholder="请选择性别">
-            <el-option label="男" value="1"></el-option>
-            <el-option label="女" value="0"></el-option>
+            <el-option label="男" :value="1"></el-option>
+            <el-option label="女" :value="0"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="出生日期" prop="birth">
@@ -29,6 +29,7 @@
               placeholder="选择日期"
               v-model="form.birth"
               style="width: 100%"
+              value-format="yyyy-MM-DD"
             ></el-date-picker>
           </el-col>
         </el-form-item>
@@ -44,7 +45,7 @@
     </el-dialog>
 
     <div class="manage-header">
-      <el-button type="text" @click="dialogVisible = true">+ 新增</el-button>
+      <el-button type="text" @click="handleAdd">+ 新增</el-button>
     </div>
 
     <el-table :data="tableData" style="width: 100%">
@@ -60,7 +61,9 @@
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
-          <el-button type="danger" size="mini" @click="handleDelete(scope.row)">删除</el-button>
+          <el-button type="danger" size="mini" @click="handleDelete(scope.row)"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -68,7 +71,7 @@
 </template>
 
 <script>
-  import { getUser } from '../api'
+  import { getUser, addUser, editUser, delUser } from '../api'
   export default {
     name: 'User',
     data() {
@@ -114,6 +117,7 @@
           ],
         },
         tableData: [],
+        modelType: 0, // 0表示新增用户，1表示编辑用户
       }
     },
     methods: {
@@ -121,7 +125,18 @@
       submit() {
         this.$refs.form.validate((val) => {
           if (val) {
-            console.log(this.form)
+            // 添加用户
+            if (this.modelType === 0) {
+              addUser(this.form).then(() => {
+                // 重新获取数据
+                this.getList()
+              })
+            } else {
+              editUser(this.form).then(() => {
+                // 重新获取数据
+                this.getList()
+              })
+            }
 
             // 提交后关闭弹窗
             this.handleClose()
@@ -138,20 +153,51 @@
       },
 
       // 编辑用户
-      handleEdit(){
-
+      handleEdit(row) {
+        this.modelType = 1
+        this.dialogVisible = true
+        this.form = JSON.parse(JSON.stringify(row))
       },
 
       // 删除用户
-      handleDelete(){
+      handleDelete(row) {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+        })
+          .then(() => {
+            delUser({ id: row.id }).then(() => {
+              this.$message({
+                type: 'success',
+                message: '删除成功!',
+              })
+              this.getList()
+            })
+          })
+          .catch(() => {
+            this.$message({
+              type: 'info',
+              message: '已取消删除',
+            })
+          })
+      },
 
-      }
+      // 获取列表数据
+      getList() {
+        getUser().then(({ data }) => {
+          // console.log(data)
+          this.tableData = data.list
+        })
+      },
+      // 添加用户
+      handleAdd() {
+        this.modelType = 0
+        this.dialogVisible = true
+      },
     },
     mounted() {
-      getUser().then(({ data }) => {
-        // console.log(data)
-        this.tableData = data.list
-      })
+      this.getList()
     },
   }
 </script>
